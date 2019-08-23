3f20ba21b1e1180f680418bb26ff74297c239a68
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-23 16:30:52 +0300

    setup the user tests

--- photobooth/tests.py
+++ photobooth/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import User,Images,tags
 
 # Create your tests here.
+class UserrTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= User(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
