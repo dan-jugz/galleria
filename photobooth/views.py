f568b2ccadd21bfd972a6f49178ce3a1008ac77b
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-23 17:40:14 +0300

    define the basic view template that takes you to the homepage of the app

--- photobooth/views.py
+++ photobooth/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Image
 
 # Create your views here.
 def home(request):
-    return HttpResponse('stalk-a-thon')
+    return HttpResponse(request,'home.html')
+
+def images_of_day(request):
+    date =dt.date.today()
+    images=Article.todays_images()
+
+    return render(request, 'all-images/today-images.html',{"date":date,'images':images})
 
