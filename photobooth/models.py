c2020ecd05e5a14063a3a410ef0bde1f708e16ed
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 02:50:25 +0300

    added the testcases for various functionalities

--- photobooth/models.py
+++ photobooth/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
         return self.first_name
@@ -26,7 +27,7 @@ class Image(models.Model):
     user = models.ForeignKey('User',on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/',default='Something')
+    article_image = models.ImageField(upload_to = 'images/',default='Something')
 
     @classmethod
     def todays_images(cls):
@@ -43,3 +44,4 @@ class Image(models.Model):
     def search_by_title(cls,search_term):
         image = cls.objects.filter(title__icontains=search_term)
         return image
+
--- photobooth/tests.py
+++ photobooth/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from .models import User,Images,tags
+import datetime as dt
 
-# Create your tests here.
 class UserTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.james= User(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.daniel= User(first_name = 'Daniel', last_name ='Njuguna', email ='njuguna@moringaschool.com')
 
     # Testing  instance
     def test_instance(self):
@@ -17,3 +17,33 @@ class UserTestClass(TestCase):
         self.james.save_user()
         users = User.objects.all()
         self.assertTrue(len(users) > 0)
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.daniel= User(first_name = 'Daniel', last_name ='Njuguna', email ='njuguna@moringaschool.com')
+        self.daniel.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_image= Image(title = 'Test Article',post = 'This is a random test Post',editor = self.daniel)
+        self.new_image.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        tags.objects.all().delete()
+        Image.objects.all().delete()
+    
+    def test_get_images_today(self):
+        today_news = Image.todays_images()
+        self.assertTrue(len(today_images)>0)
+
+    def test_get_images_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Image.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
