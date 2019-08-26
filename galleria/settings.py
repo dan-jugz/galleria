f9178a1a882b607d62249e80459ff4101ff5ed26
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 17:28:56 +0300

    remove minor bugs

--- galleria/settings.py
+++ galleria/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ng_3b(u$y58lho*uaibeph*ln&5gq!uaznk-bomvp-^@nwb#7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # ALLOWED_HOSTS = [config('ALLOWED_HOSTS', cast=Csv())]
 ALLOWED_HOSTS = []
