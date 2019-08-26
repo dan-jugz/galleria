2da8e0ccd093233be6e9033e8cd712c7f579817d
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 09:14:54 +0300

    update the pages on the urls script

--- photobooth/urls.py
+++ photobooth/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$',views.images_of_day,name='imagesToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'),
-    
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name ='image')
 ]
