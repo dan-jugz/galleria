04c8c922c5ed8d932074203f93338bbcd5cbe587
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 04:04:01 +0300

    correct the imports

--- photobooth/admin.py
+++ photobooth/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Images,tags
+from .models import User,Image,tags
 
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)
--- photobooth/views.py
+++ photobooth/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 from .models import Image
@@ -30,3 +30,29 @@ def past_days_images(request,past_date):
     images = image.days_images(date)
 
     return render(request, 'all-images/past-images.html',{"date": date,"images":images})
+
+def images_today(request):
+    date = dt.date.today()
+    news = Image.todays_images()
+    return render(request, 'all-images/today-images.html', {"date": date,"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-images/imagee.html", {"image":image})
+
