72e5e63d3418d45e3ef7a8975ff4edcc55f72c40
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 08:48:24 +0300

    add required imports

--- galleria/settings.py
+++ galleria/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config,Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ng_3b(u$y58lho*uaibeph*ln&5gq!uaznk-bomvp-^@nwb#7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS', cast=Csv())]
-
+# ALLOWED_HOSTS = [config('ALLOWED_HOSTS', cast=Csv())]
+ALLOWED_HOSTS = []
 
 # Application definition
 
