b94932b21e50866d46a1511d0ff3489a5fefdbc7
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-26 14:37:52 +0300

    import project settings from django.conf

--- photobooth/urls.py
+++ photobooth/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.image,name ='image')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
